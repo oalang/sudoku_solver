dc7540c5a9fe84ca04294f61ba7e22c06a281a14
oalang <82413592+oalang@users.noreply.github.com>
2022-11-28 20:07:17 -0500

    Correct Puzzle initialization in __init__.py

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -20,7 +20,7 @@ Example:
     ...          [0, 6, 0, 0, 0, 0, 2, 8, 0],
     ...          [0, 0, 0, 4, 1, 9, 0, 0, 5],
     ...          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
-    >>> puzzle = Puzzle.(clues)  # Build puzzle
+    >>> puzzle = Puzzle(clues)  # Build puzzle
     >>> puzzle.display()         # Display puzzle
     >>> puzzle.is_valid()        # Check puzzle validity
     >>> puzzle.solve()           # Solve puzzle
